36b3697602d6b96f275964329c950c7ab7ca4650
Manav <vermamanav117@gmail.com>
2025-01-20 17:35:55 +0530

    Redirection added

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://app.rwps.in');
 });
 Route::get('/{code}', [UrlController::class, 'redirect']);
 
