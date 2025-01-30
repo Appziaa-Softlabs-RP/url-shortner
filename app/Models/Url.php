425931d81993536eb79a4ecbda0c08af94bea0f2
Manav <vermamanav117@gmail.com>
2025-01-30 18:09:30 +0530

    Bugs fixes

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -41,30 +41,15 @@ class Url extends Model
      */
     public function create(array $data)
     {
-        // Add IP Address - Get real client IP from proxy
-        $data['ip'] = request()->header('X-Forwarded-For') ??
-            request()->header('X-Real-IP') ??
-            request()->ip();
-
-        // If multiple IPs in X-Forwarded-For, get the first one
-        if (strpos($data['ip'], ',') !== false) {
-            $data['ip'] = trim(explode(',', $data['ip'])[0]);
-        }
+        // Add IP Address
+        $data['ip'] = request()->ip();
 
         // Add Geolocation (Country, City, Location)
-        $ip = $data['ip'];
-        // Handle localhost/private IPs
-        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
-            $ip = request()->header('X-Forwarded-For') ?? '8.8.8.8'; // Try X-Forwarded-For before fallback
-        }
-
-        $location = Location::get($ip);
+        $location = Location::get($data['ip']);
         if ($location) {
-            $data['country_code'] = $location->countryCode ?? null;
-            $data['city'] = $location->cityName ?? null;
-            $data['location'] = ($location->latitude && $location->longitude)
-                ? "{$location->latitude},{$location->longitude}"
-                : null;
+            $data['country_code'] = $location->countryCode;
+            $data['city'] = $location->cityName;
+            $data['location'] = "{$location->latitude},{$location->longitude}";
         } else {
             $data['country_code'] = null;
             $data['city'] = null;
